1ebde4eea7d5c4ac89d3089b09d6eb3879c8febc
mario8912 <lopezvilamario98@gmail.com>
2024-10-02 13:52:48 +0200

    color verde a morado, oficina correos label, lista calles div border eliminado, remove list item icon anadido

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+let correosOfficeSpan = document.getElementById("correosOfficeSpan")
 let addresses = []; // Array para almacenar las direcciones
 let autocomplete;
 
-// Inicializa el autocompletado
+const correosOfficeAdressInput = document.getElementById("officeInput");
+correosOfficeAdressInput.value = "Carrer Pere el Ceremonios, 4, 12560 Benicàssim, Castelló"
+// Evento blur para manejar cuando la oficina de correos pierde el foco
+correosOfficeAdressInput.addEventListener('blur', handleBlur);
+
+function handleBlur() {
+    let correosOfficeAdressInputValue = correosOfficeAdressInput.value;
+
+    if (correosOfficeAdressInputValue != "") {
+        // Inserta el título "Oficina de Correos" y el valor ingresado si no existe ya
+            correosOfficeSpan.innerText = correosOfficeAdressInputValue
+    }
+}
+
+// Inicializa el autocompletado para el input de direcciones y el input de oficina de correos
 function initAutocomplete() {
+    // Autocompletar para el input de direcciones
     autocomplete = new google.maps.places.Autocomplete(
         document.getElementById("addressInput"),
         { types: ["geocode"] }
@@ -25,6 +41,7 @@ function initAutocomplete() {
         const place = officeAutocomplete.getPlace();
         if (place && place.formatted_address) {
             document.getElementById('officeInput').value = place.formatted_address;
+            handleBlur(); // Llama a la función handleBlur después de seleccionar la dirección
         }
     });
 }
@@ -44,14 +61,27 @@ function updateAddressList() {
     const suggestionsDiv = document.getElementById("suggestions");
     suggestionsDiv.innerHTML = ""; // Limpiar sugerencias
 
-    addresses.forEach(address => {
+    addresses.forEach((address, index) => {
         const div = document.createElement("div");
         div.className = "suggestion-item";
+        
+        // Crear el texto de la dirección
         div.innerText = address;
-        div.onclick = () => {
-            addAddress(address);
-            suggestionsDiv.style.display = "none"; // Ocultar sugerencias después de hacer clic
+
+        // Crear el botón de eliminar
+        const removeButton = document.createElement("button");
+        removeButton.innerHTML = '<img src="ico/remove.png" alt="Eliminar" style="width: 12px; height: 12px;"/> '; // Icono de cruz
+        removeButton.onclick = () => {
+            removeAddress(index); // Llama a la función para eliminar la dirección
         };
+
+        removeButton.style.background = 'transparent'; // Fondo transparente
+        removeButton.style.border = 'none'; // Sin borde
+        removeButton.style.cursor = 'pointer'; // Cambia el cursor al pasar el ratón
+        removeButton.style.marginLeft = '6px'; // Cambia el cursor al pasar el ratón
+
+        // Añadir el texto y el botón al div
+        div.appendChild(removeButton);
         suggestionsDiv.appendChild(div);
     });
 
@@ -61,7 +91,13 @@ function updateAddressList() {
         suggestionsDiv.style.display = "none"; // Ocultar si no hay direcciones
     }
 
-    document.getElementById('addressInput').value = ''; // Limpiar el input de dirección
+    document.getElementById('addressInput').value = '';
+}
+
+// Función para eliminar una dirección
+function removeAddress(index) {
+    addresses.splice(index, 1); // Elimina la dirección del array
+    updateAddressList(); // Actualiza la lista de direcciones
 }
 
 // Función para calcular y mostrar la ruta
@@ -110,8 +146,8 @@ function finalizeRoute() {
 
     // Mostrar la ruta optimizada en la página
     const resultDiv = document.getElementById("result");
-    
-    resultDiv.innerHTML = "<h2><span class='optimized-route'>Ruta Optimizada:</span></h2><ul></ul>";
+    resultDiv.style.display = "block"
+    resultDiv.innerHTML = "<h3><span class='optimized-route'>Ruta Optimizada:</span></h3><ul></ul>";
 
     const list = resultDiv.querySelector("ul");
 
